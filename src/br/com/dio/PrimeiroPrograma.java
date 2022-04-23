2aef7b46e6bfe0088ba585b25875fac933bfcbfc
Denison Tenorio <denison_tenorio@hotmail.com>
2022-04-23 18:26:03 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma { //editar o nome do programa Shift+F6
+    public static void main(String[] args) {
+        /* System.out.println("Hello Word!");*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
